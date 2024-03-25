5111ae114ba8142f942672acb481e26eaa64ed3c
SergioRamirezNavarro <ramireznavarro.sergio@gmail.com>
2024-03-25 19:50:33 +0100

    templates

--- src/main/java/com/ceica/securityspring/config/SecurityConfig.java
+++ src/main/java/com/ceica/securityspring/config/SecurityConfig.java
@@ -3,11 +3,8 @@ import com.ceica.securityspring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -40,7 +37,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/home").permitAll()
                         .requestMatchers("/","/register").permitAll()
-                        .requestMatchers("/js/**","/css/**").permitAll()
+                        .requestMatchers("/js/**", "/static/css/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
