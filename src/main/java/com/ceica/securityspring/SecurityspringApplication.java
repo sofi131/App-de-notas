a25c4e170df13c7f1c8b43bd69e62cb9f061e0d9
SergioRamirezNavarro <ramireznavarro.sergio@gmail.com>
2024-03-25 17:08:31 +0100

    comentamos generación pass

--- src/main/java/com/ceica/securityspring/SecurityspringApplication.java
+++ src/main/java/com/ceica/securityspring/SecurityspringApplication.java
@@ -9,8 +9,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class SecurityspringApplication {
 
     public static void main(String[] args) {
-        BCryptPasswordEncoder encoder=new BCryptPasswordEncoder();
-        System.out.println(encoder.encode("1234"));
+        //BCryptPasswordEncoder encoder=new BCryptPasswordEncoder();
+        //System.out.println(encoder.encode("1234"));
 
         SpringApplication.run(SecurityspringApplication.class, args);
     }
