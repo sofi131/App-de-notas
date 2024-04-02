07b8bc28255c332fe5f0a8f5adb19c2f270eeac9
sofi131 <sofiasanmartin131@gmail.com>
2024-04-02 16:41:47 +0200

    app de notas

--- src/main/java/com/ceica/securityspring/SecurityspringApplication.java
+++ src/main/java/com/ceica/securityspring/SecurityspringApplication.java
@@ -11,8 +11,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class SecurityspringApplication {
 
     public static void main(String[] args) {
-        //BCryptPasswordEncoder encoder=new BCryptPasswordEncoder();
-        //System.out.println(encoder.encode("1234"));
+//        BCryptPasswordEncoder encoder=new BCryptPasswordEncoder();
+//        System.out.println(encoder.encode("1234"));
 
 
 
--- src/main/java/com/ceica/securityspring/config/SecurityConfig.java
+++ src/main/java/com/ceica/securityspring/config/SecurityConfig.java
@@ -52,9 +52,4 @@ public class SecurityConfig {
 
         return http.build();
     }
-
-
-
-
-
 }
